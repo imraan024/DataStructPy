9ca9ae905704b63585c41ed660beac4fc602f158
Md. Al-Imran <asq24i@gmail.com>
2021-08-22 22:00:35 +0600

    prime number file uploaded

---
+++ prime.py
@@ -0,0 +1,12 @@
+def isPrime(num):
+	for i in range(2, num):
+		if (num % i == 0):
+			break
+	else:
+		return True
+
+
+for i in range(1, 20):
+	if isPrime(i + 1):
+			print(i + 1, end=" ")
+print()
